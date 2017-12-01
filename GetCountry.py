d83898aafbc536feed879729fdb9d2331bd7eb0e
Victor Belugin <victor.belugin@xyrality.com>
2017-12-01 06:45:51 +0200

    code optimization

--- GetCountry.py
+++ GetCountry.py
@@ -5,27 +5,19 @@ def get_by_country_code(country_code):
     response = requests.get(
         'http://www.webservicex.net/country.asmx/GetCountryByCountryCode?CountryCode=%s' % country_code)
 
-    fname = 'countries.xml'
-
-    fhandle = open(fname, "w")
-    fhandle.write(response.text)
-    fhandle.close()
-
-    fh = open(fname)
     countries = []
     country = ""
-    for line in fh:
-        if line.find("&lt;name&gt;") < 0: continue
-        startpos = line.find('&gt;')
-        endpos = line.find('&lt;/')
-        result = line[startpos:endpos].strip('&gt;')
+    for line in response.iter_lines():
+        if str(line).find("&lt;name&gt;") < 0: continue
+        startpos = str(line).find('&gt;')
+        endpos = str(line).find('&lt;/')
+        result = str(line)[startpos:endpos].strip('&gt;')
         if len(result) > 0:
             if not result == country:
                 country = result
                 countries.append(result)
             else:
                 continue
-    fh.close()
     if len(countries) > 1:
         return countries
     else:
@@ -36,22 +28,14 @@ def get_currency_by_country(country):
     response = requests.get(
         'http://www.webservicex.net/country.asmx/GetCurrencyByCountry?CountryName=%s' % country)
 
-    fname = 'countries.xml'
-
-    fhandle = open(fname, "w")
-    fhandle.write(response.text)
-    fhandle.close()
-
-    fh = open(fname)
     currency = ""
-    for line in fh:
-        if line.find("&lt;Currency&gt;") < 0: continue
-        startpos = line.find('&gt;')
-        endpos = line.find('&lt;/')
-        result = line[startpos:endpos].strip('&gt;')
+    for line in response.iter_lines():
+        if str(line).find("&lt;Currency&gt;") < 0: continue
+        startpos = str(line).find('&gt;')
+        endpos = str(line).find('&lt;/')
+        result = str(line)[startpos:endpos].strip('&gt;')
         if len(result) > 0:
             currency = result
-    fh.close()
     return currency
 
 
@@ -59,22 +43,15 @@ def get_currency_by_code(currency_code):
     response = requests.get(
         'http://www.webservicex.net/country.asmx/GetCountryByCurrencyCode?CurrencyCode=%s' % currency_code)
 
-    fname = 'countries.xml'
-
-    fhandle = open(fname, "w")
-    fhandle.write(response.text)
-    fhandle.close()
-
-    fh = open(fname)
     currency = ""
-    for line in fh:
-        if line.find("&lt;Currency&gt;") < 0: continue
-        startpos = line.find('&gt;')
-        endpos = line.find('&lt;/')
-        result = line[startpos:endpos].strip('&gt;')
+    for line in response.iter_lines():
+        if str(line).find("&lt;Currency&gt;") < 0: continue
+        startpos = str(line).find('&gt;')
+        endpos = str(line).find('&lt;/')
+        result = str(line)[startpos:endpos].strip('&gt;')
         if len(result) > 0:
             currency = result
-    fh.close()
+
     return currency
 
 
@@ -82,20 +59,13 @@ def get_currency_code_by_name(currency_name):
     response = requests.get(
         'http://www.webservicex.net/country.asmx/GetCurrencyCodeByCurrencyName?CurrencyName=%s' % currency_name)
 
-    fname = 'countries.xml'
-
-    fhandle = open(fname, "w")
-    fhandle.write(response.text)
-    fhandle.close()
-
-    fh = open(fname)
     currencyCodes = []
     currencyCode = ""
-    for line in fh:
-        if line.find("&lt;CurrencyCode&gt;") < 0: continue
-        startpos = line.find('&gt;')
-        endpos = line.find('&lt;/')
-        result = line[startpos:endpos].strip('&gt;')
+    for line in response.iter_lines():
+        if str(line).find("&lt;CurrencyCode&gt;") < 0: continue
+        startpos = str(line).find('&gt;')
+        endpos = str(line).find('&lt;/')
+        result = str(line)[startpos:endpos].strip('&gt;')
         if len(result) > 0:
             if not result == currencyCode:
                 currencyCode = result
@@ -103,7 +73,6 @@ def get_currency_code_by_name(currency_name):
             else:
                 continue
 
-    fh.close()
     if len(currencyCodes) > 1:
         return currencyCodes
     else:
